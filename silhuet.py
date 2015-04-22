796408170023d27921e22537a9baa0714068bdc0
Mathias Rav <m@git.strova.dk>
2015-04-22 11:43:32 +0200

    Add silhuet.py

---
+++ silhuet.py
@@ -0,0 +1,32 @@
+def merge_silhouette(A, B):
+    inf = 1e9
+    n = len(A)
+    m = len(B)
+    A[0] = 0
+    B[0] = 0
+    A[n+1] = 0
+    B[m+1] = 0
+    A[n+2] = +inf
+    B[m+2] = +inf
+    res = []
+    res[0] = 0
+    i = 1
+    j = 1
+    k = 1
+    while i <= n or j <= m:
+        if A[i] < B[j]:
+            res[k] = A[i]
+            res[k+1] = max(A[i+1], B[j-1])
+            i = i + 2
+        elif A[i] > B[j]:
+            res[k] = B[j]
+            res[k+1] = max(A[i-1], B[j+1])
+            j = j + 2
+        else:  # A[i] == B[j]
+            res[k] = A[i]
+            res[k+1] = max(A[i+1], B[j+1])
+            i = i + 2
+            j = j + 2
+        if res[k+1] != res[k-1]:
+            k = k + 2
+    return res
