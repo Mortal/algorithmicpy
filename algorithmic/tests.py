4749caf21f7ef5279f3439543c608f91e277f8ac
Mathias Rav <m@git.strova.dk>
2018-02-18 21:59:41 +0100

    Remember to close StringIO

--- algorithmic/tests.py
+++ algorithmic/tests.py
@@ -58,11 +58,11 @@ class PatternMatchTest(unittest.TestCase):
 
 class AlgorithmicpyTest(unittest.TestCase):
     def runner(self, py, tex):
-        buf = io.StringIO()
-        visitor = Visitor(print=functools.partial(print, file=buf),
-                          source=py)
-        visitor.visit(ast.parse(py).body[0])
-        self.assertEqual(buf.getvalue().rstrip('\n'), tex)
+        with io.StringIO() as buf:
+            visitor = Visitor(print=functools.partial(print, file=buf),
+                              source=py)
+            visitor.visit(ast.parse(py).body[0])
+            self.assertEqual(buf.getvalue().rstrip('\n'), tex)
 
     def test_assert(self):
         self.runner('assert 42', r'\STATE $\{42\}$')
